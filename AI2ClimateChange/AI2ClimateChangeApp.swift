d01f8d3bef9f6f2f4e6cfde29512ec1164cb6812
Sadhana <ganagdharan.sadhana@gmail.com>
2024-12-29 12:45:06 -0600

    Initial Commit

---
+++ AI2ClimateChange/AI2ClimateChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI2ClimateChangeApp.swift
+//  AI2ClimateChange
+//
+//  Created by kokila on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AI2ClimateChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI2ClimateChange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI2ClimateChange
+//
+//  Created by kokila on 12/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
